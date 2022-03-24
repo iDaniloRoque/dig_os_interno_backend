8f882c78467e3de558db449aefb3233941a7c0b4
Danilo Roque <dsr.proj3ct@gmail.com>
2022-03-24 15:31:45 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,12 @@ const { db } = require('./.env')
 
 module.exports = {
   client: 'mssql',
-        connection: db
+        connection: {
+        server: process.env.SERVER ,
+        user: process.env.USER ,
+        password: process.env.PASSWORD ,
+        database: process.env.DATABASE ,
+        port: process.env.PORT 
+        }
   };
 
